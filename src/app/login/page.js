5ee351b2e1162d4be7c202d39f89a15cea58ce50
dophat1 <dophat2403@gmail.com>
2026-05-31 16:13:03 +0200

    adding login and signup using supabase

---
+++ src/app/login/page.js
@@ -0,0 +1,32 @@
+'use client'
+
+import {useState} from 'react'
+import supabase from '../../lib/supabaseClient'
+
+export default function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    async function handleSubmit() {
+        const result = await supabase.auth.signInWithPassword({ email, password })
+        console.log(result)
+    }
+    return (
+        <div>
+            <h1>Login</h1>
+            <input 
+                type="email" 
+                placeholder="Email" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input 
+                type="password" 
+                placeholder="Password" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    )
+}
---
+++ src/app/signup/page.js
@@ -0,0 +1,32 @@
+'use client'
+
+import { useState } from 'react'   
+import supabase from '../../lib/supabaseClient'      
+
+export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function handleSubmit() {
+    const result =await supabase.auth.signUp({ email, password })
+    console.log(result)
+  }
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <input 
+        type="email" 
+        placeholder="Email" 
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input 
+        type="password" 
+        placeholder="Password" 
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  )
+}
